4672d7fe102a680deef4af91daf9a510b31995b2
Jonathan Pincas <jon@pincas.co.uk>
2020-12-05 10:17:40 +0100

    before package rearrange

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
-Functional HTML Library for Go
+## Functional HTML Library for Go
 
 A library to help you write HTML layouts as regular Go functions, rather than using a template.
 
 Heavily inspired by the way Elm does HTML output - in fact, bar a few tweaks to help suit Go's syntax a bit better, it's almost identical.
 
 Currently being developed on an 'as needed' basis, so please feel free to add missing elements if you want/need them.
+
+See the test file for a complete example of an HTML page written with `htmlfunc`
+
+## Tests
+
+Kind of tricky to test, but running `./test.sh` will generate an `index.html` and test it against the *vnu* html validator if you have it installed.
+
+Inspecting the HTML is handy development crutch, and then visually checking over the file in a browser should help confirm the output is an intended.
+
+- [VNU HTML Validator](https://github.com/validator/validator/)
+
--- html/tags.go
+++ html/tags.go
@@ -47,6 +47,8 @@ type Attribute struct {
 	IsBool    bool
 }
 
+type Attributes []Attribute
+
 func (a Attribute) RenderIf(doRender bool) Attribute {
 	if doRender {
 		return a
@@ -55,8 +57,29 @@ func (a Attribute) RenderIf(doRender bool) Attribute {
 	return Attribute{}
 }
 
-func Attrs(attrs ...Attribute) []Attribute {
+func Attrs(attrs ...Attribute) Attributes {
 	return attrs
+}
+
+// Merge helps deal with repition of certain attributes, merging them into one.
+// At the moment, we deal with 'class' and 'style'
+func (attrs Attributes) Merge() Attributes {
+	var classes []string
+	var styles []string
+	var otherAttrs Attributes
+
+	for _, attr := range atts {
+		switch attr.Name {
+		case class:
+			classes = append(classes, attr.Val)
+		case style:
+			styles = append(styles, attr.Val)
+		default:
+			otherAttrs = append(otherAttrs, attr)
+		}
+	}
+
+	return Attributes{}
 }
 
 type Element struct {
@@ -64,7 +87,7 @@ type Element struct {
 	Raw string
 
 	Tag           string
-	Attributes    []Attribute
+	Attributes    Attributes
 	Elements      []Element
 	Text          string
 	IsSelfClosing bool
@@ -106,7 +129,7 @@ func Els2(el1, el2 Element, els []Element) []Element {
 	return append(Els(el1, el2), els...)
 }
 
-func basicTag(tag string, attrs []Attribute, elements []Element) Element {
+func basicTag(tag string, attrs Attributes, elements []Element) Element {
 	return Element{
 		Tag:        tag,
 		Elements:   elements,
@@ -114,7 +137,7 @@ func basicTag(tag string, attrs []Attribute, elements []Element) Element {
 	}
 }
 
-func selfClosingTag(tag string, attrs []Attribute) Element {
+func selfClosingTag(tag string, attrs Attributes) Element {
 	return Element{
 		Tag:           tag,
 		Attributes:    attrs,
@@ -128,87 +151,87 @@ func RawElement(b []byte) Element {
 	}
 }
 
-func Div(attrs []Attribute, elements ...Element) Element {
+func Div(attrs Attributes, elements ...Element) Element {
 	return basicTag(div, attrs, elements)
 }
 
-func P(attrs []Attribute, elements ...Element) Element {
+func P(attrs Attributes, elements ...Element) Element {
 	return basicTag(p, attrs, elements)
 }
 
-func A(attrs []Attribute, elements ...Element) Element {
+func A(attrs Attributes, elements ...Element) Element {
 	return basicTag(a, attrs, elements)
 }
 
-func Span(attrs []Attribute, elements ...Element) Element {
+func Span(attrs Attributes, elements ...Element) Element {
 	return basicTag(span, attrs, elements)
 }
 
-func Section(attrs []Attribute, elements ...Element) Element {
+func Section(attrs Attributes, elements ...Element) Element {
 	return basicTag(section, attrs, elements)
 }
 
-func Img(attrs []Attribute) Element {
+func Img(attrs Attributes) Element {
 	return selfClosingTag(img, attrs)
 }
 
-func SVG(attrs []Attribute, elements ...Element) Element {
+func SVG(attrs Attributes, elements ...Element) Element {
 	return basicTag(svg, attrs, elements)
 }
 
-func Path(attrs []Attribute) Element {
+func Path(attrs Attributes) Element {
 	return basicTag(path, attrs, []Element{})
 }
 
-func Polygon(attrs []Attribute) Element {
+func Polygon(attrs Attributes) Element {
 	return basicTag(polygon, attrs, []Element{})
 }
 
-func Rect(attrs []Attribute) Element {
+func Rect(attrs Attributes) Element {
 	return basicTag(rect, attrs, []Element{})
 }
 
-func Line(attrs []Attribute) Element {
+func Line(attrs Attributes) Element {
 	return basicTag(line, attrs, []Element{})
 }
 
-func Circle(attrs []Attribute) Element {
+func Circle(attrs Attributes) Element {
 	return basicTag(circle, attrs, []Element{})
 }
 
-func Br() Element {
+func Br(_ Attributes) Element {
 	return selfClosingTag(br, []Attribute{})
 }
 
-func Nav(attrs []Attribute, elements ...Element) Element {
+func Nav(attrs Attributes, elements ...Element) Element {
 	return basicTag(nav, attrs, elements)
 }
 
-func Html(attrs []Attribute, elements ...Element) Element {
+func Html(attrs Attributes, elements ...Element) Element {
 	return basicTag(html, attrs, elements)
 }
 
-func Head(attrs []Attribute, elements ...Element) Element {
+func Head(attrs Attributes, elements ...Element) Element {
 	return basicTag(head, attrs, elements)
 }
 
-func Title(attrs []Attribute, elements ...Element) Element {
+func Title(attrs Attributes, elements ...Element) Element {
 	return basicTag(title, attrs, elements)
 }
 
-func Meta(attrs []Attribute) Element {
+func Meta(attrs Attributes) Element {
 	return selfClosingTag(meta, attrs)
 }
 
-func Body(attrs []Attribute, elements ...Element) Element {
+func Body(attrs Attributes, elements ...Element) Element {
 	return basicTag(body, attrs, elements)
 }
 
-func Link(attrs []Attribute) Element {
+func Link(attrs Attributes) Element {
 	return selfClosingTag(link, attrs)
 }
 
-func Script(attrs []Attribute, elements ...Element) Element {
+func Script(attrs Attributes, elements ...Element) Element {
 	return basicTag(script, attrs, elements)
 }
 
@@ -222,67 +245,67 @@ func Text(i interface{}) Element {
 // Nothing generates a blank element.  The only reason we have the arguments
 // is to make the funciton type signature the same as the other construction
 // functions
-func Nothing(_ []Attribute, _ ...Element) Element {
+func Nothing(_ Attributes, _ ...Element) Element {
 	return Element{}
 }
 
-func Button(attrs []Attribute, elements ...Element) Element {
+func Button(attrs Attributes, elements ...Element) Element {
 	return basicTag(button, attrs, elements)
 }
 
-func Table(attrs []Attribute, elements ...Element) Element {
+func Table(attrs Attributes, elements ...Element) Element {
 	return basicTag(table, attrs, elements)
 }
 
-func THead(attrs []Attribute, elements ...Element) Element {
+func THead(attrs Attributes, elements ...Element) Element {
 	return basicTag(thead, attrs, elements)
 }
 
-func TBody(attrs []Attribute, elements ...Element) Element {
+func TBody(attrs Attributes, elements ...Element) Element {
 	return basicTag(tbody, attrs, elements)
 }
 
-func Tr(attrs []Attribute, elements ...Element) Element {
+func Tr(attrs Attributes, elements ...Element) Element {
 	return basicTag(tr, attrs, elements)
 }
 
-func Th(attrs []Attribute, elements ...Element) Element {
+func Th(attrs Attributes, elements ...Element) Element {
 	return basicTag(th, attrs, elements)
 }
 
-func Td(attrs []Attribute, elements ...Element) Element {
+func Td(attrs Attributes, elements ...Element) Element {
 	return basicTag(td, attrs, elements)
 }
 
-func H1(attrs []Attribute, elements ...Element) Element {
+func H1(attrs Attributes, elements ...Element) Element {
 	return basicTag(h1, attrs, elements)
 }
 
-func H2(attrs []Attribute, elements ...Element) Element {
+func H2(attrs Attributes, elements ...Element) Element {
 	return basicTag(h2, attrs, elements)
 }
 
-func H3(attrs []Attribute, elements ...Element) Element {
+func H3(attrs Attributes, elements ...Element) Element {
 	return basicTag(h3, attrs, elements)
 }
 
-func H4(attrs []Attribute, elements ...Element) Element {
+func H4(attrs Attributes, elements ...Element) Element {
 	return basicTag(h4, attrs, elements)
 }
 
-func H5(attrs []Attribute, elements ...Element) Element {
+func H5(attrs Attributes, elements ...Element) Element {
 	return basicTag(h5, attrs, elements)
 }
 
-func H6(attrs []Attribute, elements ...Element) Element {
+func H6(attrs Attributes, elements ...Element) Element {
 	return basicTag(h6, attrs, elements)
 }
-func Ul(attrs []Attribute, elements ...Element) Element {
+func Ul(attrs Attributes, elements ...Element) Element {
 	return basicTag(ul, attrs, elements)
 }
-func Ol(attrs []Attribute, elements ...Element) Element {
+func Ol(attrs Attributes, elements ...Element) Element {
 	return basicTag(ol, attrs, elements)
 }
-func Li(attrs []Attribute, elements ...Element) Element {
+func Li(attrs Attributes, elements ...Element) Element {
 	return basicTag(li, attrs, elements)
 }
